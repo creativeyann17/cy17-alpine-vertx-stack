18e12df96469d8130bc4ac67e13add4b93c53346
Yann MARCOU <creativeyann17@gmail.com>
2023-12-13 01:27:06 -0500

    rolling specific

--- rolling.sh
+++ rolling.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 docker-compose stop app-1
-docker-compose up --build -d
+docker-compose up --build -d app-1
 sleep 30
 docker-compose stop app-2
-docker-compose up --build -d
+docker-compose up --build -d app-2
